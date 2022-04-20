555e44159d9854e033600d3ef7e5f283a136e6b7
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-04-20 08:47:00 -0400

    NFA test overlapping states

---
+++ test/test_nfa.py
@@ -0,0 +1,22 @@
+import unittest
+
+from nfa import NFA
+
+
+class TestNFA(unittest.TestCase):
+    def test_overlapping_states(self):
+        """
+        The operations for union and concatenation cannot be performed if the NFAs have
+        any states in common.
+        """
+        q1, q2 = object(), object()
+
+        a = NFA(({q1, q2}, {"a"}, {(q1, "a"): {q2}}, q1, {q2}))
+        b = NFA(({q1, q2}, {"b"}, {(q1, "b"): {q2}}, q1, {q2}))
+
+        self.assertRaises(ValueError, a.concat, b)
+        self.assertRaises(ValueError, a.union, b)
+
+
+if __name__ == "__main__":
+    unittest.main()
