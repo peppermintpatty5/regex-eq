a0c0155a265044ca58aa0c177a2a2f5e082b8828
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2022-04-21 09:04:45 -0400

    Add and update docstrings
    - Turn class Token into a dataclass

--- regular.py
+++ regular.py
@@ -1,3 +1,10 @@
+"""
+Classes relating to regular languages and regular expression parsing.
+
+`Regular` is the only class from this module that you should be using.
+"""
+
+from dataclasses import dataclass
 from enum import Enum
 from string import printable
 from typing import Optional, Union
@@ -8,7 +15,20 @@ from nfa import NFA
 
 class Regular:
     """
-    A regular language, which is associated with a DFA.
+    A regular language, which is associated with a DFA. Objects of this class behave
+    like sets of strings in that you can:
+
+    - Check if a string is `in` the language
+    - Perform the following operations:
+        - `|` Union
+        - `&` Intersection
+        - `~` Complement
+        - `-` Relative complement
+        - `^` Symmetric difference
+    - Check for the following relations:
+        - `<` Subset (proper)
+        - `==` Equality
+    - Check for emptiness by using `bool` (implicitly or explicitly)
     """
 
     def __init__(self, dfa: DFA) -> None:
@@ -19,7 +39,7 @@ class Regular:
         """
         Construct a regular language from a regular expression.
         """
-        return Parser(regex).parse_expr().eval()
+        return Parser(regex).parse().eval()
 
     @staticmethod
     def from_finite(language: set[str]) -> "Regular":
@@ -88,8 +108,17 @@ class Regular:
         return NotImplemented
 
 
+@dataclass
 class Token:
+    """
+    A lexical unit consisting of a type and the corresponding lexeme.
+    """
+
     class Type(Enum):
+        """
+        Enumeration of token types
+        """
+
         DOT = 0
         STAR = 1
         PLUS = 2
@@ -103,9 +132,8 @@ class Token:
         END = 10
         ERROR = 11
 
-    def __init__(self, type: Type, lexeme: str) -> None:
-        self.type = type
-        self.lexeme = lexeme
+    type: Type
+    lexeme: str
 
 
 class Lexer:
@@ -117,7 +145,15 @@ class Lexer:
         self._input = iter(regex)
 
     def next_token(self) -> Token:
+        """
+        Get the next token
+        """
+
         class State(Enum):
+            """
+            States of the lexical analyzer
+            """
+
             START = 0
             ESCAPE = 1
 
@@ -163,15 +199,22 @@ class Lexer:
                             return Token(Token.Type.SYMBOL, c)
 
 
-class Operator(Enum):
-    UNION = 0
-    CONCAT = 1
-    STAR = 2
-    PLUS = 3
-    QUESTION = 4
-
-
 class Node:
+    """
+    A node in a syntax tree
+    """
+
+    class Operator(Enum):
+        """
+        Enumeration of operators
+        """
+
+        UNION = 0
+        CONCAT = 1
+        STAR = 2
+        PLUS = 3
+        QUESTION = 4
+
     def __init__(
         self,
         val: Union[Operator, Token],
@@ -186,9 +229,11 @@ class Node:
         return repr({k: v for k, v in self.__dict__.items() if v is not None})
 
     def eval(self) -> Regular:
-        if type(self.val) is Operator:
+        """
+        Evaluate the syntax tree to produce a regular language
+        """
+        if isinstance(self.val, Node.Operator):
             operator = self.val
-
             a = self.left.eval() if self.left is not None else None
             b = self.right.eval() if self.right is not None else None
 
@@ -196,22 +241,20 @@ class Node:
                 raise Exception("missing left operand")
 
             match operator:
-                case Operator.UNION:
+                case Node.Operator.UNION:
                     return a | b
-                case Operator.CONCAT:
+                case Node.Operator.CONCAT:
                     return a + b
-                case Operator.STAR:
+                case Node.Operator.STAR:
                     return Regular(DFA.from_NFA(a.dfa.star()))
-                case Operator.PLUS:
+                case Node.Operator.PLUS:
                     return a + Regular(DFA.from_NFA(a.dfa.star()))
-                case Operator.QUESTION:
+                case Node.Operator.QUESTION:
                     return a | Regular.from_finite({""})
                 case _:
                     raise ValueError("invalid operator")
-
-        elif type(self.val) is Token:
+        else:
             token = self.val
-
             match token.type:
                 case Token.Type.SYMBOL:
                     return Regular.from_finite({token.lexeme})
@@ -219,9 +262,6 @@ class Node:
                     return Regular.from_finite(set(printable))
                 case _:
                     raise ValueError("invalid operand")
-
-        else:
-            raise Exception("how did this happen?")
 
 
 class Parser:
@@ -233,71 +273,71 @@ class Parser:
         self._lexer = Lexer(regex)
         self._pushback: Optional[Token] = None
 
-    def next_token(self) -> Token:
+    def _next_token(self) -> Token:
         if self._pushback is not None:
             x = self._pushback
             self._pushback = None
             return x
         return self._lexer.next_token()
 
-    def pushback_token(self, token: Token) -> None:
+    def _pushback_token(self, token: Token) -> None:
         if self._pushback is None:
             self._pushback = token
         else:
             raise Exception("pushback error")
 
-    def parse_expr(self) -> Node:
-        term1 = self.parse_term()
+    def _parse_expr(self) -> Node:
+        term1 = self._parse_term()
 
         if term1 is None:
             raise Exception("missing term")
 
         while True:
-            token = self.next_token()
+            token = self._next_token()
 
             if token.type is not Token.Type.UNION:
-                self.pushback_token(token)
+                self._pushback_token(token)
                 break
 
-            term2 = self.parse_term()
+            term2 = self._parse_term()
             if term2 is None:
                 raise Exception("missing expression after '|'")
 
-            term1 = Node(Operator.UNION, term1, term2)
+            term1 = Node(Node.Operator.UNION, term1, term2)
 
         return term1
 
-    def parse_term(self) -> Optional[Node]:
-        factor1 = self.parse_factor()
+    def _parse_term(self) -> Optional[Node]:
+        factor1 = self._parse_factor()
 
         if factor1 is None:
             return None
 
         while True:
-            factor2 = self.parse_factor()
+            factor2 = self._parse_factor()
             if factor2 is None:
                 break
-            factor1 = Node(Operator.CONCAT, factor1, factor2)
+            factor1 = Node(Node.Operator.CONCAT, factor1, factor2)
 
         return factor1
 
-    def parse_factor(self) -> Optional[Node]:
-        token = self.next_token()
+    def _parse_factor(self) -> Optional[Node]:
+        token = self._next_token()
 
         if token.type in (Token.Type.SYMBOL, Token.Type.DOT):
             expr = Node(token)
         elif token.type is Token.Type.L_PAREN:
-            expr = self.parse_expr()
+            expr = self._parse_expr()
             if expr is None:
                 raise Exception("syntax error")
-            if self.next_token().type is not Token.Type.R_PAREN:
+            if self._next_token().type is not Token.Type.R_PAREN:
                 raise Exception("missing ')'")
         else:
-            self.pushback_token(token)
+            self._pushback_token(token)
             return None
 
         while True:
-            exponent = self.parse_exponent()
+            exponent = self._parse_exponent()
             if exponent is None:
                 break
 
@@ -306,16 +346,22 @@ class Parser:
 
         return expr
 
-    def parse_exponent(self) -> Optional[Node]:
-        token = self.next_token()
+    def _parse_exponent(self) -> Optional[Node]:
+        token = self._next_token()
 
         match token.type:
             case Token.Type.STAR:
-                return Node(Operator.STAR)
+                return Node(Node.Operator.STAR)
             case Token.Type.PLUS:
-                return Node(Operator.PLUS)
+                return Node(Node.Operator.PLUS)
             case Token.Type.QUESTION:
-                return Node(Operator.QUESTION)
+                return Node(Node.Operator.QUESTION)
             case _:
-                self.pushback_token(token)
+                self._pushback_token(token)
                 return None
+
+    def parse(self) -> Node:
+        """
+        Parse the regular expression to produce a syntax tree
+        """
+        return self._parse_expr()
